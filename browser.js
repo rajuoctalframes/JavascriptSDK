9a7a250609280dc3a8007b81f8673f188d948d79
sandeep kumar <sandeeputtam807@gmail.com>
2021-02-08 10:37:56 +0530

    moved for https

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 if (typeof window==="undefined") {
   throw new Error("This SDK is Only for browsers.");
 }
-(function(t,a,s,k,p){k=t.Promise;s=a.createElement("script");s.src="http://chatmonday.com/utm/talk_init.js";s.async = true;a.head.appendChild(s);
+(function(t,a,s,k,p){k=t.Promise;s=a.createElement("script");s.src="https://chatmonday.com/utm/talk_init.js";s.async = true;a.head.appendChild(s);
 	t.CML=false;t.Talk={v:1 , ready:{then:async function(f){ if(!t.CML && t.Talk.User === undefined){p=new k((res,rej)=>{s.onload=res;s.onerror=rej;t.Loaded=true;});
 	await k.all([p]);}if(typeof f=="function")f();}}};})(window,document);
 var Talk = window.Talk;
